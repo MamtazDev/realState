568de504ba812ccc293edcaaddeaa60bcde9e7f8
HannanBhuiyan <hannan.diuh@gmail.com>
2023-04-10 14:36:08 +0600

    show frontend news and category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
     public function index(){
-        return view('frontend.home');
+        $testmonials = Testimonial::all();
+        $all_news = News::with('relationwithNewsCategory')->take(3)->get();
+        return view('frontend.home', compact('testmonials', 'all_news'));
     }
+     
 
 }
